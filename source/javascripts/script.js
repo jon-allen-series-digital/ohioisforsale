eda1011f5ec1af890b71f99ee213cf01fc498bed
Jonathan Allen <jonathanallen@Jons-MacBook-Pro.local>
2019-05-12 16:23:30 -0400

    add scroll to top button

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -104,3 +104,19 @@ $('.container').infiniteScroll({
   history: 'replace',
   hideNav: '.pagination',
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
