6a5be933758685208af8de8f771d61d7b80c2a21
Jonathan Allen <jonathanallen@Jons-MacBook-Pro.local>
2019-05-13 08:30:02 -0400

    scroll position of sidenav

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -7,73 +7,6 @@ $(window).resize(function() {
 	$('.inner-wrap, #offcanvas').removeClass('active');
 });
 
-$.fn.preload = function() {
-    this.each(function(){
-        $('<img/>')[0].src = this;
-    });
-}
-var imageLoad = $('.preload a').attr('href');
-imageLoad = imageLoad.toString();
-
-$(["/images/logo.png"]).preload();
-
-if ($(".page-view").hasClass("vl-one")) {
-	$(["/images/pages/vacationland-one/vacationland_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("vl-two")) {
-	$(["/images/pages/vacationland-two/vacationland_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("vl-three")) {
-	$(["/images/pages/vacationland-three/vacationland_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-one")) {
-	$(["/images/pages/chapter-one/ohio_page" + imageLoad + "_gray.gif"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-two")) {
-	$(["/images/pages/chapter-two/ohio2_gray" + imageLoad + ".jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-three")) {
-	$(["/images/pages/chapter-three/ohio3_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-four")) {
-	$(["/images/pages/chapter-four/ohio4_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-five")) {
-	$(["/images/pages/chapter-five/ohio5_" + imageLoad + "_grey.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-six")) {
-	$(["/images/pages/chapter-six/ohio_6_" + imageLoad + "_grey.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-seven")) {
-	$(["/images/pages/chapter-seven/ohio_7_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-eight")) {
-	$(["/images/pages/chapter-eight/ohio_8_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-nine")) {
-	$(["/images/pages/chapter-nine/ohio_9_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-ten")) {
-	$(["/images/pages/chapter-ten/ohio_10_" + imageLoad + "_gray.png"]).preload();
-}
-else if ($(".page-view").hasClass("ohio-eleven")) {
-	$(["/images/pages/chapter-eleven/ohio_11_" + imageLoad + "_gray.jpg"]).preload();
-}
-else if ($(".page-view").hasClass("lonesome-era-one")) {
-	$(["/images/lonesome-era/1/lonesome_era_1_" + imageLoad + ".jpg"]).preload();
-}
-else if ($(".page-view").hasClass("lonesome-era-two")) {
-	$(["/images/lonesome-era/2/lonesome_era_2_" + imageLoad + ".jpg"]).preload();
-}
-else if ($(".page-view").hasClass("lonesome-era-three")) {
-	$(["/images/lonesome-era/3/lonesome_era_3_" + imageLoad + ".jpg"]).preload();
-}
-else if ($(".page-view").hasClass("lonesome-era-four")) {
-	$(["/images/lonesome-era/4/lonesome_era_4_" + imageLoad + ".jpg"]).preload();
-}
-else if ($(".page-view").hasClass("lonesome-era-five")) {
-	$(["/images/lonesome-era/5/lonesome_era_5_" + imageLoad + ".jpg"]).preload();
-}
 
 // nav link highlighting
 
@@ -106,25 +39,30 @@ $('.container').infiniteScroll({
   prefill: true,
 });
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
 
-$(window).scroll(function () {
-	$('#offcanvas ul').offset == 0;
+$(window).scroll(function(){
+    var scrollPos = $(document).scrollTop();
+    $('#offcanvas ul').css("margin-top", scrollPos + 'px');
 });
+
+
+	
+
 
 });
 
